c45fd60c3f0c8b773ce5182a579485556758167e
tjarco <info@tjarcokerssens.nl>
2014-09-24 18:28:03 +0200

    List geimplementeerd

--- src/assignment2/List.java
+++ src/assignment2/List.java
@@ -1,76 +1,137 @@
 package assignment2;
 
 public class List<E extends Data> implements ListInterface<E> {
+	private Node<E> list;
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return list.next == null;
 	}
 
 	@Override
 	public List<E> init() {
-		// TODO Auto-generated method stub
-		return null;
+		list = null;
+		return this;
 	}
 
 	@Override
 	public List<E> clone() {
-		return null;
+		setFirst();
+		List<E> clone = new List<E>();
+		while (list.next != null) {
+			clone.insert(list.next.clone().data);
+			list = list.next;
+		}
+		return clone;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(list == null){
+			return 0;
+		}
+		Node<E> temp = list;
+		setFirst();
+		int size = 1;
+		while (list.next != null) {
+			size++;
+			list = list.next;
+		}
+		list = temp;
+		return size;
 	}
 
 	@Override
 	public List<E> insert(E d) {
-		// TODO Auto-generated method stub
-		return null;
+		if (list == null) {
+			list = new Node<E>(d);
+		} else if (list.next == null) {
+			list = list.next = new Node<E>(d, list, null);
+		} else {
+			list = list.next = list.next.prior = new Node<E>(d, list, list.next);
+		}
+		return this;
 	}
 
 	@Override
 	public E retrieve() {
-		// TODO Auto-generated method stub
-		return null;
+		return list.data;
 	}
 
 	@Override
 	public List<E> remove() {
-		// TODO Auto-generated method stub
-		return null;
+		if (list.prior == null) {
+			list = list.next;
+			list.prior = null;
+		} else if (list.next == null) {
+			list = list.prior;
+			list.next = null;
+		} else {
+			list.prior.next = list.next;
+			list.next.prior = list.prior;
+			list = list.next;
+		}
+		return this;
 	}
 
 	@Override
 	public boolean find(E d) {
-		// TODO Auto-generated method stub
+		if (!setFirst())
+			return false;
+
+		if (list.data.compareTo(d) == 0) {
+			return true;
+		}
+		while (list.next != null) {
+			list = list.next;
+
+			if (list.data.compareTo(d) == 0) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public boolean setFirst() {
-		// TODO Auto-generated method stub
-		return false;
+		if (list == null) {
+			return false;
+		}
+		while (list.prior != null) {
+			list = list.prior;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean setLast() {
-		// TODO Auto-generated method stub
-		return false;
+		if (list == null) {
+			return false;
+		}
+
+		while (list.next != null) {
+			list = list.next;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean getNext() {
-		// TODO Auto-generated method stub
-		return false;
+		if (list == null || list.next == null) {
+			return false;
+		}
+
+		list = list.next;
+		return true;
 	}
 
 	@Override
 	public boolean getPrior() {
-		// TODO Auto-generated method stub
-		return false;
+		if (list == null || list.prior == null) {
+			return false;
+		}
+		list = list.prior;
+		return true;
 	}
 
 }
